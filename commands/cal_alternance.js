60bbd6bb03f803af5604da76017cc6208345171a
AkLeStream <mercierhugo@gmail.com>
2021-10-21 11:33:01 +0200

    Command /cal - OK

--- commands/cal_alternance.js
+++ commands/cal_alternance.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { CALENDRIER_ALTERNANCE_L, CALENDRIER_ALTERNANCE_M1, CALENDRIER_ALTERNANCE_M2 } = require('./config.json');
+const { CAL_L, CAL_M1, CAL_M2 } = require('../config.json');
+const {MessageEmbed} = require("discord.js");
 
 
 module.exports =
@@ -7,23 +8,44 @@ module.exports =
     data: new SlashCommandBuilder()
         .setName('cal')
         .setDescription('envoie le calendrier d\'alternance')
-        .addUserOption(option => option
+        .addStringOption(option => option
             .setName('annee')
             .setDescription('Année du calendrier, li, m1 ou m2')
             .setRequired(true)),
     async execute(interaction)
     {
-        let annee = interaction.options.getUser('annee');
+        let annee = interaction.options.getString('annee')
         switch (annee)
         {
             case "li":
-                interaction.reply('Calendrier d\'alternace de Licence',{files : [CALENDRIER_ALTERNANCE_L]});
+                if(CAL_L==="PATH")
+                {
+                    interaction.reply({ content: 'Calendrier d\'alternace de Licence n\'est pas trouver ou manquant', ephemeral: true, files: [] });
+                }
+                else
+                {
+                    interaction.reply({content:'Calendrier d\'alternace de Licence', ephemeral: true, files: [CAL_L] });
+                }
                 break;
             case "m1":
-                interaction.reply('Calendrier d\'alternace de Master 1',{files : [CALENDRIER_ALTERNANCE_M1]});
+                if(CAL_M1==="PATH")
+                {
+                    interaction.reply({ content: 'Calendrier d\'alternace de Master 1 n\'est pas trouver ou manquant', ephemeral: true, files: [] });
+                }
+                else
+                {
+                    interaction.reply({content:'Calendrier d\'alternace de Master 1', ephemeral: true, files: [CAL_M1] });
+                }
                 break;
             case "m2":
-                interaction.reply('Calendrier d\'alternace de Master 2',{files : [CALENDRIER_ALTERNANCE_M2]});
+                if(CAL_M2==="PATH")
+                {
+                    interaction.reply({ content: 'Calendrier d\'alternace de Master 2 n\'est pas trouver ou manquant', ephemeral: true, files: [] });
+                }
+                else
+                {
+                    interaction.reply({content:'Calendrier d\'alternace de Master 2', ephemeral: true, files: [CAL_M2] });
+                }
                 break;
         }
     },
