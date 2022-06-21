923ed65c0ef6bce3b9a6a936edaf9911aaadbf7e
AkLeStream <50136090+AkLeCode@users.noreply.github.com>
2022-06-21 11:14:22 +0200

    Ajout de /help (provisoire) et suppretion de /del

---
+++ commands/help.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+const {MessageEmbed} = require("discord.js");
+
+permission= new Permissions(Permissions.FLAGS.ADMINISTRATOR);
+
+// TODO check utility
+
+module.exports =
+    {
+        data: new SlashCommandBuilder()
+            .setName('help')
+            .setDescription('Responds with a list of commands'),
+        async execute(interaction)
+        {
+            replyOptions.content = `/edt url : Get edt web url\n
+            /edt teams_url : Get team current main url\n
+            \n
+            /resource logo : Give requested logo\n
+            /resources calendar : Give requested calendar in asked format\n
+            /resources links : The link you want\n
+            \n
+            /self manage check : Check your account into our service\n
+            /self manage remove : Remove your account from our service\n
+            /self manage update : Update your account (firstname, lastname)\n
+            /self register : Register yourself into our service if is not already done`;
+
+        }
+
+    }
