e4090d6726b996ff9b84cb92e0d4257ec9af21ab
AkLeStream <50136090+AkLeCode@users.noreply.github.com>
2022-06-21 13:25:45 +0200

    up /help

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports =
         async execute(_interaction)
         {
             _interaction.reply({content: `
-            /edt url : Get edt web url\n/edt teams_url : Get team current main url\n/resource logo : Give requested logo\n/resources calendar : Give requested calendar in asked format\n/resources links : The link you want\n/self manage check : Check your account into our service\n/self manage remove : Remove your account from our service\n/self manage update : Update your account (firstname, lastname)\n/self register : Register yourself into our service if is not already done`, ephemeral: true});
+            /edt url : Get edt web url\n/edt teams_url : Get team current main url\n\n/resource logo : Give requested logo\n/resources calendar : Give requested calendar in asked format\n/resources links : The link you want\n\n/self manage check : Check your account into our service\n/self manage remove : Remove your account from our service\n/self manage update : Update your account (firstname, lastname)\n/self register : Register yourself into our service if is not already done`, ephemeral: true});
 
         }
 
